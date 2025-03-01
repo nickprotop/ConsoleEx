d0208c7eb4e3a3c41ab65cdee348e3b265164e4c
Nick Protopapas <nikolaos.protopapas@gmail.com>
2025-03-01 19:36:10 +0200

    Update Program.cs and simplify readme.md content
    Uncommented the instantiation of `SystemInfoWindow` in `Program.cs` while commenting out `ClockWindow`, shifting focus to system information display.
    
    In `readme.md`, removed the detailed section on key components and classes of the ConsoleEx window system, streamlining the documentation and reducing complexity.

--- Example/Program.cs
+++ Example/Program.cs
@@ -53,9 +53,9 @@ namespace ConsoleEx.Example
 			var consoleWindowSystem = InitializeConsoleWindowSystem();
 
 			var logWindow = new LogWindow(consoleWindowSystem);
-			//var systemInfoWindow = new SystemInfoWindow(consoleWindowSystem);
+			var systemInfoWindow = new SystemInfoWindow(consoleWindowSystem);
 			var userInfoWindow = new UserInfoWindow(consoleWindowSystem);
-			// var clockWindow = new ClockWindow(consoleWindowSystem);
+			//var clockWindow = new ClockWindow(consoleWindowSystem);
 
 			try
 			{
