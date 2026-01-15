bf53559cfb53dd81ae466105e58756c2ede3f7bf
Nikolaos Protopapas <nikolaos.protopapas@gmail.com>
2026-01-15 20:48:43 +0200

    Simplify plugin system architecture
    - Remove PluginManager, IPluginManager, and FileSystemPluginDiscovery
    - Simplify IPlugin interface with base PluginBase class
    - Plugin management now integrated directly into ConsoleWindowSystem

--- SharpConsoleUI/Plugins/IPlugin.cs
+++ SharpConsoleUI/Plugins/IPlugin.cs
@@ -2,178 +2,119 @@
 // ConsoleEx - A simple console window system for .NET Core
 //
 // Author: Nikolaos Protopapas
-// Email: nikolaos.protopapas@Gmail.com
+// Email: nikolaos.protopapas@gmail.com
 // License: MIT
 // -----------------------------------------------------------------------
 
-using Microsoft.Extensions.DependencyInjection;
 using SharpConsoleUI.Controls;
 using SharpConsoleUI.Themes;
 
 namespace SharpConsoleUI.Plugins;
 
 /// <summary>
-/// Plugin information record
+/// Metadata about a plugin
 /// </summary>
 /// <param name="Name">The plugin name</param>
 /// <param name="Version">The plugin version</param>
 /// <param name="Author">The plugin author</param>
-/// <param name="Description">The plugin description</param>
-/// <param name="Dependencies">Plugin dependencies</param>
-public sealed record PluginInfo(
-    string Name,
-    string Version,
-    string Author,
-    string Description,
-    IReadOnlyList<string> Dependencies
-);
+/// <param name="Description">A description of what the plugin provides</param>
+public record PluginInfo(string Name, string Version, string Author, string Description);
 
 /// <summary>
-/// Base interface for all SharpConsoleUI plugins
+/// A theme contribution from a plugin
 /// </summary>
-public interface IPlugin : IDisposable, IAsyncDisposable
+/// <param name="Name">The unique name for the theme</param>
+/// <param name="Description">A description of the theme</param>
+/// <param name="Theme">The theme instance</param>
+public record PluginTheme(string Name, string Description, ITheme Theme);
+
+/// <summary>
+/// A control factory contribution from a plugin
+/// </summary>
+/// <param name="Name">The unique name for the control</param>
+/// <param name="Factory">A factory function that creates control instances</param>
+public record PluginControl(string Name, Func<IWindowControl> Factory);
+
+/// <summary>
+/// A window/dialog factory contribution from a plugin
+/// </summary>
+/// <param name="Name">The unique name for the window</param>
+/// <param name="Factory">A factory function that creates window instances</param>
+public record PluginWindow(string Name, Func<ConsoleWindowSystem, Window> Factory);
+
+/// <summary>
+/// A service contribution from a plugin
+/// </summary>
+/// <param name="ServiceType">The service type (usually an interface)</param>
+/// <param name="Instance">The service instance</param>
+public record PluginService(Type ServiceType, object Instance);
+
+/// <summary>
+/// Simple plugin interface. Plugins return what they provide via Get* methods.
+/// </summary>
+public interface IPlugin : IDisposable
 {
-    /// <summary>
-    /// Gets the plugin information
-    /// </summary>
-    PluginInfo Info { get; }
+	/// <summary>
+	/// Gets the plugin metadata
+	/// </summary>
+	PluginInfo Info { get; }
 
-    /// <summary>
-    /// Initializes the plugin
-    /// </summary>
-    /// <param name="services">The service provider</param>
-    /// <param name="cancellationToken">Cancellation token</param>
-    /// <returns>A task representing the initialization operation</returns>
-    Task InitializeAsync(IServiceProvider services, CancellationToken cancellationToken = default);
+	/// <summary>
+	/// Gets the themes provided by this plugin
+	/// </summary>
+	/// <returns>A list of themes, or empty if the plugin provides no themes</returns>
+	IReadOnlyList<PluginTheme> GetThemes();
 
-    /// <summary>
-    /// Configures services for the plugin
-    /// </summary>
-    /// <param name="services">The service collection</param>
-    void ConfigureServices(IServiceCollection services);
+	/// <summary>
+	/// Gets the control factories provided by this plugin
+	/// </summary>
+	/// <returns>A list of control factories, or empty if the plugin provides no controls</returns>
+	IReadOnlyList<PluginControl> GetControls();
 
-    /// <summary>
-    /// Gets whether the plugin is initialized
-    /// </summary>
-    bool IsInitialized { get; }
+	/// <summary>
+	/// Gets the window/dialog factories provided by this plugin
+	/// </summary>
+	/// <returns>A list of window factories, or empty if the plugin provides no windows</returns>
+	IReadOnlyList<PluginWindow> GetWindows();
+
+	/// <summary>
+	/// Gets the services provided by this plugin
+	/// </summary>
+	/// <returns>A list of services, or empty if the plugin provides no services</returns>
+	IReadOnlyList<PluginService> GetServices();
+
+	/// <summary>
+	/// Called when the plugin is loaded, before Get* methods are called.
+	/// Use this for any initialization that requires access to the window system.
+	/// </summary>
+	/// <param name="windowSystem">The window system that loaded the plugin</param>
+	void Initialize(ConsoleWindowSystem windowSystem);
 }
 
 /// <summary>
-/// Interface for control plugins that provide custom controls
-/// </summary>
-public interface IControlPlugin : IPlugin
-{
-    /// <summary>
-    /// Gets the types of controls provided by this plugin
-    /// </summary>
-    IReadOnlyList<Type> ControlTypes { get; }
-
-    /// <summary>
-    /// Creates a control instance of the specified type
-    /// </summary>
-    /// <param name="controlType">The control type</param>
-    /// <param name="services">The service provider</param>
-    /// <returns>The created control instance</returns>
-    IWindowControl CreateControl(Type controlType, IServiceProvider services);
-}
-
-/// <summary>
-/// Interface for theme plugins that provide custom themes
-/// </summary>
-public interface IThemePlugin : IPlugin
-{
-    /// <summary>
-    /// Gets the themes provided by this plugin
-    /// </summary>
-    IReadOnlyDictionary<string, Type> ThemeTypes { get; }
-
-    /// <summary>
-    /// Creates a theme instance of the specified type
-    /// </summary>
-    /// <param name="themeName">The theme name</param>
-    /// <param name="services">The service provider</param>
-    /// <returns>The created theme instance</returns>
-    ITheme CreateTheme(string themeName, IServiceProvider services);
-}
-
-/// <summary>
-/// Base abstract plugin class providing common functionality
+/// Base class for plugins with default empty implementations.
+/// Inherit from this class and override only the methods you need.
 /// </summary>
 public abstract class PluginBase : IPlugin
 {
-    private bool _disposed;
+	/// <inheritdoc/>
+	public abstract PluginInfo Info { get; }
 
-    /// <inheritdoc />
-    public abstract PluginInfo Info { get; }
+	/// <inheritdoc/>
+	public virtual IReadOnlyList<PluginTheme> GetThemes() => Array.Empty<PluginTheme>();
 
-    /// <inheritdoc />
-    public bool IsInitialized { get; private set; }
+	/// <inheritdoc/>
+	public virtual IReadOnlyList<PluginControl> GetControls() => Array.Empty<PluginControl>();
 
-    /// <summary>
-    /// Gets the service provider after initialization
-    /// </summary>
-    protected IServiceProvider? Services { get; private set; }
+	/// <inheritdoc/>
+	public virtual IReadOnlyList<PluginWindow> GetWindows() => Array.Empty<PluginWindow>();
 
-    /// <inheritdoc />
-    public virtual async Task InitializeAsync(IServiceProvider services, CancellationToken cancellationToken = default)
-    {
-        if (IsInitialized)
-            return;
+	/// <inheritdoc/>
+	public virtual IReadOnlyList<PluginService> GetServices() => Array.Empty<PluginService>();
 
-        Services = services ?? throw new ArgumentNullException(nameof(services));
+	/// <inheritdoc/>
+	public virtual void Initialize(ConsoleWindowSystem windowSystem) { }
 
-        await OnInitializingAsync(cancellationToken);
-        IsInitialized = true;
-        await OnInitializedAsync(cancellationToken);
-    }
-
-    /// <inheritdoc />
-    public virtual void ConfigureServices(IServiceCollection services)
-    {
-        // Base implementation does nothing
-    }
-
-    /// <summary>
-    /// Called during plugin initialization before IsInitialized is set to true
-    /// </summary>
-    /// <param name="cancellationToken">Cancellation token</param>
-    /// <returns>A task representing the initialization operation</returns>
-    protected virtual Task OnInitializingAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-    /// <summary>
-    /// Called after plugin initialization when IsInitialized is set to true
-    /// </summary>
-    /// <param name="cancellationToken">Cancellation token</param>
-    /// <returns>A task representing the initialization operation</returns>
-    protected virtual Task OnInitializedAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-    /// <summary>
-    /// Called during disposal
-    /// </summary>
-    protected virtual void OnDisposing() { }
-
-    /// <summary>
-    /// Called during async disposal
-    /// </summary>
-    protected virtual ValueTask OnDisposingAsync() => ValueTask.CompletedTask;
-
-    /// <inheritdoc />
-    public void Dispose()
-    {
-        if (_disposed) return;
-
-        OnDisposing();
-        _disposed = true;
-        GC.SuppressFinalize(this);
-    }
-
-    /// <inheritdoc />
-    public async ValueTask DisposeAsync()
-    {
-        if (_disposed) return;
-
-        await OnDisposingAsync();
-        _disposed = true;
-        GC.SuppressFinalize(this);
-    }
+	/// <inheritdoc/>
+	public virtual void Dispose() { }
 }
