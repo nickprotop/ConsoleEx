49502dc2c4cb30dd1fd4e3c1b5a7fc277602b582
Nikolaos Protopapas <nikolaos.protopapas@gmail.com>
2026-02-22 11:44:39 +0200

    Remove DotNetIDE example project

--- Examples/DotNetIDE/Program.cs
+++
@@ -1,6 +0,0 @@
-if (SharpConsoleUI.PtyShim.RunIfShim(args)) Environment.Exit(127);
-
-var projectPath = args.Length > 0 ? Path.GetFullPath(args[0]) : Directory.GetCurrentDirectory();
-
-using var app = new DotNetIDE.IdeApp(projectPath);
-app.Run();
